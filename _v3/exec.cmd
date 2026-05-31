c036769100f6245f59440eb92a56b9236b6cdb8e
amolla01 <amolla01@yahoo.com>
2026-05-31 00:00:16 -0500

    Update exec.cmd

--- _v3/exec.cmd
+++ _v3/exec.cmd
@@ -677,3 +677,6 @@ ssh Exit_Router1 '/log print where topics~"route|routing|bgp"'
 ssh Exit_Router2 '/log print where topics~"route|routing|bgp"'
 QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
 ssh Exit_Router1 '/routing bgp session print count-only'; ssh Exit_Router1 '/routing bgp session print terse'; ssh Exit_Router2 '/routing bgp session print count-only'; ssh Exit_Router2 '/routing bgp session print terse'
+QQQQQQQQQQQQQQQ
+/routing bgp connection print count-only for configured peers
+/routing bgp session print count-only for live sessions
