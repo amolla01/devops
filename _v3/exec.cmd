89e205b5ca64cb813da5bc30610d9ed0e6d3246c
amolla01 <amolla01@yahoo.com>
2026-05-30 22:18:13 -0500

    Update exec.cmd

--- _v3/exec.cmd
+++ _v3/exec.cmd
@@ -641,3 +641,10 @@ ssh Exit_Router1 '/routing bgp connection print detail; /routing bgp session pri
 ssh Exit_Router2 '/routing bgp connection print detail; /routing bgp session print detail'
 ssh Border_Leaf1 "sudo vtysh -c 'show bgp neighbors 10.0.253.1'"
 ssh Border_Leaf2 "sudo vtysh -c 'show bgp neighbors 10.0.253.3'"
+CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+ssh Border_Leaf1 "sudo vtysh -c 'show bgp neighbors 10.0.253.1'"
+ssh Border_Leaf2 "sudo vtysh -c 'show bgp neighbors 10.0.253.3'"
+ssh Exit_Router1 '/routing bgp session print detail'
+ssh Exit_Router2 '/routing bgp session print detail'
+
+
